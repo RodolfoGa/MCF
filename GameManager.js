7d5171121e0e4210aa8b23c47e47ae4ca8ab8516
Miliano DeHoyos <dehoyosmm@gmail.com>
2018-11-28 14:59:27 -0600

    Added fill for canvas

--- GameManager.js
+++ GameManager.js
@@ -4,3 +4,8 @@
 var canvas = document.querySelector(".myCanvas");
 var width = canvas.width = window.innerWidth;
 var height = canvas.height = window.innerHeight;
+
+var ctx = canvas.getContext('2d');
+
+ctx.fillStyle = 'rgb(0,0,0)';
+ctx.fillRect(0, 0, width, height);
