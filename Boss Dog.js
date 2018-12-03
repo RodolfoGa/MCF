eab5c1caa859199b33ca9e2a820118ac79debc4b
RodolfoGa <RodolfoGa@users.noreply.github.com>
2018-12-03 08:23:00 -0600

    deleted files

--- Boss Dog.js
+++
@@ -1,9 +0,0 @@
-function BossDog(){
-    constructor(name, health, damage){
-    this.name = name;
-    this.health = health;
-    this.damage = damage;
-    }
-}
-    var bossdog = new Object("Big Doggo", 1000, 50);
-    
