c910802eb5a955304d0f3afa2131028b47a2bfd1
RodolfoGa <RodolfoGa@users.noreply.github.com>
2018-12-03 08:09:53 -0600

    chnages

---
+++ Boss Dog.js
@@ -0,0 +1,9 @@
+function BossDog(){
+    constructor(name, health, damage){
+    this.name = name;
+    this.health = health;
+    this.damage = damage;
+    }
+}
+    var bossdog = new Object("Big Doggo", 1000, 50);
+    
---
+++ Dog.js
@@ -0,0 +1,9 @@
+class Dog(){
+    constructor(name, health, damage){
+    	this.name = name;
+    	this.health = health;
+    	this.damage = damage;
+    }
+}
+    var dog = new Object("Doggo", 100, 10);
+
