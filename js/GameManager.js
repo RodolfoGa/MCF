36522ab58c5e49ab8a6a5b2a064e43adb112b1bb
Miliano DeHoyos <dehoyosmm@gmail.com>
2018-12-03 10:03:01 -0600

    Added movement and map

--- js/GameManager.js
+++ js/GameManager.js
@@ -1,6 +1,6 @@
 /**
- *
  */
+
 let canvas = document.querySelector(".myCanvas");
 let width = canvas.width = window.innerWidth;
 let height = canvas.height = window.innerHeight;
@@ -11,27 +11,32 @@ function degToRad(degrees) {
 	  return degrees * Math.PI / 180;
 	};
 
-ctx.fillStyle = 'rgb(0, 0, 0)';
-ctx.fillRect(0, 0, width, height);
-
-ctx.translate(width/2, height/2);
+let sprite = 0;
+let posX = width/2 + 85 ;
+let posY = height/2 + 250;
+let refreshrate = 13;
 
 let image = new Image();
 image.src = 'img/walk-right.png';
-image.onload = drawRight;
 
 let image2 = new Image();
 image2.src = 'img/walk-left.png';
-image.onload = drawLeft;
 
-let sprite = 0;
-let posX = 0;
-let refreshrate = 13;
+let bgImage = new Image();
+bgImage.src = 'img/level2.png';
 
+window.onload = function(){
+	ctx.fillStyle = 'rgb(0, 0, 0)';
+	ctx.fillRect(0, 0, width, height);
+	ctx.translate(width/2, height/2);
+	ctx.drawImage(bgImage, -posX, -posY);
+	ctx.drawImage(image, (sprite*102), 0, 102, 148, 0, -74, 51, 74);
+}
 
 function drawRight() {
 	ctx.fillRect(-(width/2), -(height/2), width, height);
-	ctx.drawImage(image, (sprite*102), 0, 102, 148, 0+posX, -74, 102, 148);
+	ctx.drawImage(bgImage, -posX, -posY);
+	ctx.drawImage(image, (sprite*102), 0, 102, 148, 0, -74, 51, 74);
 	if (posX % refreshrate === 0) {
 	    if (sprite === 5) {
 	      sprite = 0;
@@ -39,19 +44,14 @@ function drawRight() {
 	      sprite++;
 	    }
 	  }
-	if(posX > width/2) {
-	    newStartPos = -((width/2) + 102);
-	    posX = Math.ceil(newStartPos / refreshrate) * refreshrate;
-	    console.log(posX);
-	  } else {
 	    posX += 6;
 	    console.log(posX);
-	  }
 };
 
 function drawLeft() {
 	ctx.fillRect(-(width/2), -(height/2), width, height);
-	ctx.drawImage(image2, (sprite*102), 0, 102, 148, 0+posX, -74, 102, 148);
+	ctx.drawImage(bgImage, -posX, -posY);
+	ctx.drawImage(image2, (sprite*102), 0, 102, 148, 0, -74, 54, 74);
 	if (posX % refreshrate === 0) {
 	    if (sprite === 0) {
 	      sprite = 5;
@@ -59,14 +59,36 @@ function drawLeft() {
 	      sprite--;
 	    }
 	  }
-	if(posX < -(width/2)-64) {
-	    newStartPos = ((width/2) + 102);
-	    posX = Math.ceil(newStartPos / refreshrate) * refreshrate;
-	    console.log(posX);
-	  } else {
 	    posX -= 6;
 	    console.log(posX);
+};
+
+function drawUp() {
+	ctx.fillRect(-(width/2), -(height/2), width, height);
+	ctx.drawImage(bgImage, -posX, -posY);
+	ctx.drawImage(image2, (sprite*102), 0, 102, 148, 0, -74, 54, 74);
+	if (posX % refreshrate === 0) {
+	    if (sprite === 0) {
+	      sprite = 5;
+	    } else {
+	      sprite--;
+	    }
 	  }
+	    posY -= 6;
+};
+
+function drawDown() {
+	ctx.fillRect(-(width/2), -(height/2), width, height);
+	ctx.drawImage(bgImage, -posX, -posY);
+	ctx.drawImage(image2, (sprite*102), 0, 102, 148, 0, -74, 54, 74);
+	if (posX % refreshrate === 0) {
+	    if (sprite === 0) {
+	      sprite = 5;
+	    } else {
+	      sprite--;
+	    }
+	  }
+	    posY += 6;
 };
 
 let keyState = {};
@@ -83,6 +105,12 @@ function gameLoop() {
     }
     if (keyState[39] || keyState[68]){
     	window.requestAnimationFrame(drawRight);
+		}
+		if (keyState[38] || keyState[87]){
+    	window.requestAnimationFrame(drawUp);
+		}
+		if (keyState[40] || keyState[83]){
+    	window.requestAnimationFrame(drawDown);
     }
     setTimeout(gameLoop, 10);
 }
